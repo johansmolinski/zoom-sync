fbabda35a9413588d6edc7419a0618dda7c46fed
Johan Smolinski <git.js@crypt.se>
2019-02-28 15:57:50 +0100

    Breaking out logics to small functions, adding tests

--- main/pulse.cpp
+++ main/pulse.cpp
@@ -1,32 +1,15 @@
 #include "pulse.h"
+#include <stdio.h>
 
 #define read_in(register_in, bit_in) (*register_in & 1 << bit_in)
 #define set_high(register_out, bit_out) (*register_out |= 1 << bit_out)
 #define set_low(register_out, bit_out) (*register_out &= ~(1 << bit_out))
 
-void pulse(state_t *state) {
-  // Flank reset
-  if (read_in(state->register_in, state->bit_in) == 0) {
-    state->flank_read = 0;
-  }
+void makePulse(state_t *state) {
+  state->sync_on = PULSE_TIME;
+}
 
-  // Flank triggered, restart Tempo counting
-  if (read_in(state->register_in, state->bit_in) && state->flank_read == 0) {
-    state->flank_read = 1;
-    state->tempo = state->measure_counter / MULTIPLYER;
-    state->sync_on = PULSE_TIME;
-    state->measure_counter = 0;
-    state->pulse_counter = 1;
-  }
-
-  // Counter reaching divided Tempo length, trig Pulse
-  if (state->beat_counter == state->tempo && state->pulse_counter < MULTIPLYER) {
-    state->sync_on = PULSE_TIME;
-    state->beat_counter = 0;
-    state->pulse_counter++;
-  }
-
-  // Pulse is triggered, emit led
+void syncManager(state_t *state) {
   if (state->sync_on) {
     set_high(state->register_out, state->bit_out);
     state->sync_on--;
@@ -35,8 +18,55 @@ void pulse(state_t *state) {
   else {
     set_low(state->register_out, state->bit_out);
     //Serial.println("Off");
-  }  
+  }
+}
 
+void flankResetOnPulseLow(state_t *state) {
+  if (read_in(state->register_in, state->bit_in) == 0) {
+    state->flank_read = 0;
+  }
+}
+
+void startHostBeat(state_t *state) {
+    state->tempo = state->measure_counter / MULTIPLYER;
+    makePulse(state);
+    state->measure_counter = 0;
+    state->pulse_counter = MULTIPLYER - 1;
+    state->beat_counter = 0;
+}
+
+bool flankTriggerOnPulseHigh(state_t *state) {
+  if (read_in(state->register_in, state->bit_in) && state->flank_read == 0) {
+    state->flank_read = 1;
+    return true;
+  }
+  return false;
+}
+
+void beatDividerTrig(state_t *state) {
   state->beat_counter++;
+  if (state->beat_counter == state->tempo && state->pulse_counter) {
+    makePulse(state);
+    state->beat_counter = 0;
+    state->pulse_counter--;
+  }
+}
+
+void pulse(state_t *state) {
+  // Flank reset
+  flankResetOnPulseLow(state);
+
+  // Flank trigger check
+  if (flankTriggerOnPulseHigh(state)) {
+    // Triggered!
+    startHostBeat(state);
+  }
+
+  // Counter reaching divided Tempo length, trig Pulse
+  beatDividerTrig(state);
+
+  // Pulse is triggered, emit led
+  syncManager(state);
+
   state->measure_counter++;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,87 @@
 #include "../main/pulse.cpp"
 #include <gtest/gtest.h>
 
-TEST(Foo, Bar) {
-  ASSERT_EQ(1, 1);
+#define setup() \
+  uint8_t REG_OUT = 0, REG_IN = 0;\
+  state_t state;\
+  state.register_in = &REG_IN;\
+  state.bit_in = 2;\
+  state.register_out = &REG_OUT;\
+  state.bit_out = 5
+
+#define run(f, n) for(int i=0; i<n; i++) f(&state)
+#define flank_high() REG_IN = 0xff
+#define flank_low() REG_IN = 0x00
+
+TEST(Pulse, FlankReset) {
+  setup();
+
+  ASSERT_EQ(0, state.flank_read); // Flank not triggered
+  flankResetOnPulseLow(&state);
+  ASSERT_FALSE(flankTriggerOnPulseHigh(&state));
+  ASSERT_EQ(0, state.flank_read); // Flank not triggered
+  flank_high();
+  flankResetOnPulseLow(&state);
+  ASSERT_TRUE(flankTriggerOnPulseHigh(&state));
+  ASSERT_NE(0, state.flank_read); // Flank triggered
+  flank_low();
+  flankResetOnPulseLow(&state);
+  flankTriggerOnPulseHigh(&state);
+  ASSERT_EQ(0, state.flank_read); // Flank not triggered
+}
+
+TEST(Pulse, SyncOnAndOff) {
+  setup();
+
+  ASSERT_EQ(0, state.sync_on); // No pulse counter
+  ASSERT_EQ(0, REG_OUT); // Sync is off
+  makePulse(&state);
+  syncManager(&state);
+  ASSERT_NE(0, state.sync_on); // Pulse is on
+  ASSERT_NE(0, REG_OUT); // Sync is on
+
+  for (int i = 0; i < PULSE_TIME; i++) {
+    ASSERT_NE(0, REG_OUT); // Sync is on
+    syncManager(&state);
+  }
+  ASSERT_EQ(0, REG_OUT); // Sync is off
+}
+
+TEST(Pulse, StartHostBeat) {
+  setup();
+  state.measure_counter = 200;
+
+  startHostBeat(&state);
+  ASSERT_NE(0, state.sync_on); // Pulse is on
+  ASSERT_EQ(50, state.tempo); // Tempo is Measure Counter divided by MULTIPLYER
+}
+
+TEST(Pulse, BeatDivider) {
+  setup();
+  state.measure_counter = 100;
+  state.beat_counter = 24;
+
+  startHostBeat(&state);
+  beatDividerTrig(&state);
+  ASSERT_EQ(1, state.beat_counter);
+}
+
+TEST(Pulse, Pulse) {
+  setup();
+
+  int loops = 10;
+
+  flank_high();
+  run(pulse, loops);
+  flank_low();
+  run(pulse, loops); // 20 loops so far
+  ASSERT_EQ(2 * loops, state.measure_counter);
+
+  flank_high();
+  run(pulse, loops);
+  ASSERT_EQ(5, state.tempo);
+  //ASSERT_EQ(5, state.beat_counter);
+  ASSERT_EQ(1, state.pulse_counter);
 }
 
 int main(int argc, char **argv) {
