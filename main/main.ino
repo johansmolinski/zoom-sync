613a6e7a3e486026725b5b8a4fa1fccf96c0850c
Johan Smolinski <git.js@crypt.se>
2019-02-28 12:37:22 +0100

    Removing generated comments

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,6 @@
 state_t state;
 
 void setup() {
-  // put your setup code here, to run once:
 
   DDRD &= ~(0b1100); // PD2 and PD3 inputs
   PORTD &= ~(0b1100);
@@ -14,7 +13,6 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
   //Serial.println(String(PIND & 0x04));
 
   pulse(&state, &PIND, &PORTB, 2, 5);
