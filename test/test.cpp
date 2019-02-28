7e404c52639a124cce2c5f4cc01d151448489b56
Johan Smolinski <git.js@crypt.se>
2019-02-28 11:15:00 +0100

    Introducing GoogleTest

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+#include "../main/pulse.cpp"
+#include <gtest/gtest.h>
+
+TEST(Foo, Bar) {
+  ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
